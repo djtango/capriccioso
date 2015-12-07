10d5a1958c0632104a1f409bdefb6bd66f058a77
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-12-07 18:00:11 +0000

    play two notes at same time

--- public/js/messing.js
+++ public/js/messing.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 	MIDI.loadPlugin({
 		soundfontUrl: "./js/midi/instruments/",
-		instrument: "acoustic_grand_piano",
+		instrument: "harpsichord",
 		onprogress: function(state, progress) {
 			console.log(state, progress);
 		},
@@ -9,19 +9,13 @@ window.onload = function () {
       console.log('success!');
 			var delay = 0; // play one note every quarter second
 			var note = 78; // the MIDI note
-			var note2 = 50;
 			var velocity = 127; // how hard the note hits
-			var instrument = 0;
+			var instrument = 6;
 			// play the note
 			MIDI.programChange(0, instrument);
 			MIDI.setVolume(0, 127);
 			MIDI.noteOn(0, note, velocity, delay);
-			MIDI.noteOff(0, note, delay + 1.5);
-			MIDI.programChange(1, instrument);
-			MIDI.setVolume(1, 127);
-			MIDI.noteOn(1, note2, velocity, delay);
-			MIDI.noteOff(1, note2, delay + 1.5);
-
+			MIDI.noteOff(0, note, delay + 0.75);
 		}
 	});
 };
