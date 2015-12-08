c08d3915000d4f35b7c0341649f1c1205c929608
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-12-08 11:34:43 +0000

    refactored midi.js

--- public/js/midi.js
+++ public/js/midi.js
@@ -1,30 +1,4 @@
-var playNotes = function (note1,note2) {
-	MIDI.loadPlugin({
-		soundfontUrl: "./js/midi/instruments/",
-		instrument: "acoustic_grand_piano",
-		onprogress: function(state, progress) {
-			console.log(state, progress);
-		},
-		onsuccess: function() {
-      console.log('success!');
-			var delay = 0; // play one note every quarter second
-			// var note = 55; // the MIDI note
-			// var note2 = 30;
-			var velocity = 127; // how hard the note hits
-			var instrument = 0;
-			// play the note
-			MIDI.programChange(0, instrument);
-			MIDI.programChange(1, instrument);
-			MIDI.setVolume(0, 127);
-			MIDI.setVolume(1, 127);
-			MIDI.noteOn(0, note1, velocity, delay);
-			MIDI.noteOn(1, note2, velocity, delay);
-			MIDI.noteOff(0, note1, delay + 1.5);
-			MIDI.noteOff(1, note2, delay + 1.5);
-
-		}
-	});
-};
+var instrument;
 
 var playInterval = function (note1,intervalAbove) {
 	MIDI.loadPlugin({
@@ -36,18 +10,18 @@ var playInterval = function (note1,intervalAbove) {
 			var velocity = 127; // how hard the note hits
 			var instrument = 0;
 			// play the note
-			MIDI.programChange(0, instrument);
-			MIDI.programChange(1, instrument);
-			MIDI.setVolume(0, 127);
-			MIDI.setVolume(1, 127);
-			MIDI.noteOn(0, note1, velocity, delay);
-			MIDI.noteOn(1, note2, velocity, delay);
-			MIDI.noteOff(0, note1, delay + 1.5);
-			MIDI.noteOff(1, note2, delay + 1.5);
-
+			callNote(0,note1,velocity,instrument);
+			callNote(1,note2,velocity,instrument);
 		}
 	});
 };
+
+function callNote(channel,note,velocity,instrument){
+	MIDI.programChange(channel, instrument);
+	MIDI.setVolume(channel,velocity);
+	MIDI.noteOn(channel,note,velocity,0);
+	MIDI.noteOff(channel,note,1.5);
+}
 
 var intervals = {
 	'Minor 2nd': 1,
