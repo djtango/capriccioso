f6cd4a1fb40034f0205da928df2f0869cc57dba1
Deon Tan <deonjtan@gmail.com>
2015-12-08 00:52:56 +0000

    Implemented button to play notes

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+var capri = angular.module('Capriccioso', ['ngResource']);
---
+++ public/js/capricciosoController.js
@@ -0,0 +1,8 @@
+capri.controller('CapricciosoController', [function() {
+  var self = this;
+  // var midiJS = require('./midi.js')
+  self.playNotes = function() {
+    console.log('click');
+    playNotes();
+  };
+}]);
