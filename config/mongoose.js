6ceffe6e7f7963db28308890ebc1618e21e032cf
thisdotrob <thisdotrob@gmail.com>
2015-12-09 11:16:41 +0000

    Ready to add passport

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,5 +3,6 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
   var db = mongoose.connect(config.db);
+  require('../app/models/user.server.model');
   return db;
 };
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var http = require('http');
 var path = require('path');
 var config = require('./config/config');
 var mongoose = require('./config/mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 var db = mongoose();
 var server = http.createServer(app);
+var users = require('./app/controllers/users.server.controller');
 
 server.listen(config.port);
 
@@ -14,6 +16,14 @@ console.log(process.env.NODE_ENV + ' server running at http://localhost:' + conf
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.route('/users').post(users.create).get(users.list);
