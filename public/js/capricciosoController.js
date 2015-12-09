cb9f7d21eae5befb42b98bb348a06dcdb191a614
Deon Tan <deonjtan@gmail.com>
2015-12-09 00:33:56 +0000

    Implemented interval generation and answer submission

--- public/js/capricciosoController.js
+++ public/js/capricciosoController.js
@@ -1,8 +1,8 @@
-capri.controller('CapricciosoController', ['MidiPlayer', function(MidiPlayer) {
+capri.controller('CapricciosoController', ['MidiPlayer', '$scope', function(MidiPlayer, $scope) {
   var self = this;
   self.playNotes = function() {
     console.log('click');
-    MidiPlayer.playInterval(50,5);
+    MidiPlayer.playInterval(self.currentNote,self.currentInterval);
   };
 
   self.genNote = function() {
@@ -23,9 +23,14 @@ capri.controller('CapricciosoController', ['MidiPlayer', function(MidiPlayer) {
   };
 
   self.newInterval = function() {
-      self.currentNote = self.genNote();
-      self.currentInterval = self.genInterval();
-      self.setAnswer();
+    console.log('newInterval')
+    self.currentNote = self.genNote();
+    self.currentInterval = self.genInterval();
+    self.setAnswer();
+    self.randomAnswers();
+    console.log('NI-curNote:' + self.currentNote);
+    console.log('NI-curIn:' + self.currentInterval);
+    console.log('NI-corAns:' + self.correctAnswer);
   };
 
   self.supplyAnswer = function() {
@@ -34,4 +39,27 @@ capri.controller('CapricciosoController', ['MidiPlayer', function(MidiPlayer) {
       return true;
     } else { return false; }
   };
+
+  self.randomAnswers = function() {
+    var intervals = MidiPlayer.intervals;
+    return {
+      "answer1": intervals[self.currentInterval],
+      "answer2": intervals[(self.currentInterval + 1)],
+      "answer3": intervals[(self.currentInterval + 2)],
+      "answer4": intervals[(self.currentInterval + 3)]
+    };
+  };
+
+  self.clickAnswer = function(num) {
+    var playerAnswer = "answer" + num
+    self.enteredAnswer = self.randomAnswers()[playerAnswer];
+    self.supplyAnswer();
+  };
+
+
+  $scope.init = function() {
+    console.log('$scope.init')
+    self.newInterval();
+  };
+  $scope.init();
 }]);
