4f89216e6ff73cfb7fd4759dccd5e8fdf12ca961
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-12-09 12:47:50 +0000

    working mvp

--- public/js/capricciosoController.js
+++ public/js/capricciosoController.js
@@ -1,8 +1,12 @@
 capri.controller('CapricciosoController', ['MidiPlayer', '$scope', function(MidiPlayer, $scope) {
   var self = this;
+  var intervalsValues;
+  var multipleChoice;
+  var intervals;
+
   self.playNotes = function() {
     console.log('click');
-    MidiPlayer.playInterval(self.currentNote,self.currentInterval);
+    MidiPlayer.playInterval(self.currentNote, self.currentInterval);
   };
 
   self.genNote = function() {
@@ -14,19 +18,21 @@ capri.controller('CapricciosoController', ['MidiPlayer', '$scope', function(Midi
   };
 
   self.setAnswer = function() {
-    self.correctAnswer = MidiPlayer.intervals[self.currentInterval]
+    self.correctAnswer = MidiPlayer.intervals[self.currentInterval];
   };
 
-  self.isAnswerCorrect = function(){
+  self.isAnswerCorrect = function() {
     console.log(self.enteredAnswer);
     return self.correctAnswer === self.enteredAnswer;
   };
 
   self.newInterval = function() {
-    console.log('newInterval')
+    console.log('newInterval');
     self.currentNote = self.genNote();
     self.currentInterval = self.genInterval();
     self.setAnswer();
+    self.intervalsValues = self.copyArray(MidiPlayer.intervalNamesArray);
+    self.populateAnswers();
     self.randomAnswers();
     console.log('NI-curNote:' + self.currentNote);
     console.log('NI-curIn:' + self.currentInterval);
@@ -34,31 +40,81 @@ capri.controller('CapricciosoController', ['MidiPlayer', '$scope', function(Midi
   };
 
   self.supplyAnswer = function() {
-    if(self.isAnswerCorrect()) {
+    if (self.isAnswerCorrect()) {
       self.newInterval();
       return true;
-    } else { return false; }
+    } else {
+      return false;
+    }
   };
 
   self.randomAnswers = function() {
-    var intervals = MidiPlayer.intervals;
     return {
-      "answer1": intervals[self.currentInterval],
-      "answer2": intervals[(self.currentInterval + 1)],
-      "answer3": intervals[(self.currentInterval + 2)],
-      "answer4": intervals[(self.currentInterval + 3)]
+      "answer1": self.multipleChoice[0],
+      "answer2": self.multipleChoice[1],
+      "answer3": self.multipleChoice[2],
+      "answer4": self.multipleChoice[3],
     };
   };
 
   self.clickAnswer = function(num) {
-    var playerAnswer = "answer" + num
+    var playerAnswer = "answer" + num;
     self.enteredAnswer = self.randomAnswers()[playerAnswer];
     self.supplyAnswer();
   };
 
+  self.populateAnswers = function() {
+    self.intervals = MidiPlayer.intervals;
+    self.multipleChoice = [];
+    self.populateMultipleChoice();
+    console.log("self.multipleChoice:"+self.multipleChoice);
+  };
+
+  self.populateMultipleChoice = function() {
+    var indexToDelete = self.intervalsValues.indexOf(self.intervals[self.currentInterval]);
+    console.log('indexToDelete:' + indexToDelete);
+    self.intervalsValues.splice(indexToDelete, 1);
+    console.log('self.intervalsValues:' + self.intervalsValues);
+    self.multipleChoice.push(self.correctAnswer);
+
+    for (i = 0; i < 3; i++) {
+      var rand0to9 = Math.floor((Math.random() * (10 - i)));
+      var intervalToPush = self.intervalsValues[rand0to9];
+      self.multipleChoice.push(intervalToPush);
+      self.intervalsValues.splice(rand0to9,1);
+    }
+    self.shuffleArray(self.multipleChoice);
+  };
+
+  self.shuffleArray = function(array) {
+    var i = 0,
+      j = 0,
+      temp = null;
+
+    for (i = array.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  self.copyArray = function(array) {
+    return array.map(function(element) {
+      return element;
+    });
+  };
+
+  // self.objectValuesArray = function(object) {
+  //   var array = [];
+  //   for (var o in object) {
+  //     array.push(object[o]);
+  //   }
+  //   return array;
+  // };
 
   $scope.init = function() {
-    console.log('$scope.init')
+    console.log('$scope.init');
     self.newInterval();
   };
   $scope.init();
